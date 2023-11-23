154dd5088851186b0317fb723f497c9a43ec3b17
Leonardo Denavito <jounandavito@gmail.com>
2023-11-23 19:14:44 +0700

    Added maps page & route

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,12 @@
 import About from '../views/pages/about';
 import Main from '../views/pages/main';
+import Maps from '../views/pages/maps';
 
 const routes = {
   '/': Main,
   '/main': Main,
   '/about': About,
+  '/maps': Maps,
 };
 
 export default routes;
---
+++ src/scripts/views/pages/maps.js
@@ -0,0 +1,13 @@
+const Maps = {
+  async render() {
+    return `
+          <h1>Ini adalah Map Page</h1>
+        `;
+  },
+
+  async afterRender() {
+    // after render mainpage
+  },
+};
+
+export default Maps;
