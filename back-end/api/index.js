2a28686e8c8cb28ed65f1405c8c4a11f30650060
Leonardo Denavito <jounandavito@gmail.com>
2023-12-12 12:46:07 +0700

    Revret usage of swagger

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 
-const { swaggerUi, specs } = require('./swagger');
-
 const app = express();
 
 app.use(express.json());
 
-app.use('/', routes, swaggerUi.serve);
-app.get('/', swaggerUi.setup(specs));
+app.use('/', routes);
 
 module.exports = app;
