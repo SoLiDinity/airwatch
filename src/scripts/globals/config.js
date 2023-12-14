88dc7f747ea87da62fbdff738a4a4e30596e7cfa
Leonardo Denavito <jounandavito@gmail.com>
2023-12-14 20:53:22 +0700

    Updated datasource API base url

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   WAQI_BASE_URL: 'https://api.waqi.info/',
-  AIRWATCH_ARTICLES_BASE_URL: 'https://airwatch-back-end.vercel.app',
+  AIRWATCH_ARTICLES_BASE_URL: 'https://airwatchid-back-end.vercel.app',
   TOKEN: process.env.WAQI_API_KEY,
 };
 
