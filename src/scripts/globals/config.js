e133a9d209352501f3d3c61b4133aefe9253d4be
Leonardo Denavito <127974449+SoLiDinity@users.noreply.github.com>
2025-03-21 15:48:33 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   WAQI_BASE_URL: 'https://api.waqi.info/',
-  AIRWATCH_ARTICLES_BASE_URL: 'https://blogs-api.airwatchid.site',
+  AIRWATCH_ARTICLES_BASE_URL: 'https://airwatchid-back-end.vercel.app/',
   TOKEN: process.env.WAQI_API_KEY,
 };
 
